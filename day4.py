ec5a0547c60ec25d27ef17e21c2dc2c65430b3bd
samarthmali05 <167402881+samarthmali05@users.noreply.github.com>
2024-04-18 14:00:34 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,29 @@
+#Assignment operators
+x = 200
+print(x)
+
+x += 100
+print(x)
+
+x -= 50
+print(x)
+
+x *= 2
+print(x)
+
+x /= 20
+print(x)
+
+x %= 10
+print(x)
+
+#comparison operators
+a = 10
+b = 20
+print(a>b)
+print(a<b)
+print(a==b)
+print(a>=b)
+print(a<=b)
+
+
